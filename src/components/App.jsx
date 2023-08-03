b29cb992bb1b2f612d7e35b6d8b0a19ae19481bf
Denys Kovtun <denys.kovtun@gmail.com>
2023-08-03 22:51:20 +0300

    initial files added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,82 @@
+import React from "react";
+import { Container, MainHeader, SubHeader } from './App.styled';
+import { ContactForm } from "./ContactForm/ContactForm";
+import { Filter } from "./Filter/Filter";
+import { ContactList } from "./ContactList/ContactList";
+import { nanoid } from 'nanoid';
+import Notiflix from 'notiflix';
+import { useState } from "react";
+import { defaultContacts } from "./data/DefaultContacts";
+import { useLocalStorage } from "../Hooks/useLocalStorage";
+
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+  
+  const [contacts, setContacts] = useLocalStorage('contacts', defaultContacts);
+  const [filter, setFilter] = useState('');
+  
+
+  const onAddBtnClick = FormData => {
+    const { name, number } = FormData;
+    const id = nanoid();
+
+    const includesName = contacts.find(
+      contact => contact.name.toLocaleLowerCase() === name.toLocaleLowerCase()
+    );
+
+    if (includesName) {
+      return Notiflix.Notify.warning(
+        `${name} is already in contacts`
+      );
+    } else {
+      const contact = { id, name, number };
+      setContacts(prevContacts => [...prevContacts, contact]);
+      Notiflix.Notify.success(
+        `${name} was successfully added to your contacts`
+      );
+    }
+  };
+
+  const onDeleteBtnClick = (id, name) => {
+    setContacts(prevContacts =>
+      prevContacts.filter(contact => contact.id !== id)
+    );
+    Notiflix.Notify.info(
+      `${name} was successfully deleted from your contacts`
+    );
+  };
+
+  const onFilterChange = event => {
+    setFilter(event.target.value);
+  };
+
+  const filterContacts = () => {
+    const query = filter.toLocaleLowerCase();
+
+    const filteredContacts = contacts.filter(contact =>
+      contact.name.toLocaleLowerCase().includes(query)
+    );
+
+    if (query && !filteredContacts.length) {
+      Notiflix.Notify.warning(
+        'No contacts matching your request'
+      );
+    }
+
+    return filteredContacts;
+  };
+
+ 
+    return (
+      <Container>
+        <MainHeader>Phonebook</MainHeader>
+        <ContactForm onAddBtnClick={onAddBtnClick} />
+        <SubHeader>Contacts</SubHeader>
+        <Filter value={filter} onChange={onFilterChange} />
+        <ContactList
+          contacts={filterContacts()}
+          onDeleteBtnClick={onDeleteBtnClick}
+        />
+      </Container>
+    );
 };
---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import { DebounceInput } from 'react-debounce-input';
+import { FilterWrapper } from './Filter.styled';
+import { Label } from 'components/ContactForm/ContactForm.styled';
+
+const FilterInput = styled(DebounceInput)`
+  min-width: 50%;
+  border-radius: 8px;
+  font-size: 20px;
+  padding: 12px 16px;
+  margin-left: 16px;
+  box-shadow: inset 2px 2px 5px #babecc, inset -5px -5px 10px #fff;
+`;
+
+export const Filter = ({ value, onChange }) => {
+  return (
+    <FilterWrapper>
+      <Label>
+        Find contacts by name
+        <FilterInput
+          type="text"
+          name="filter"
+          placeholder="Enter contact to search"
+          value={value}
+          onChange={onChange}
+          debounceTimeout={500}
+        />
+      </Label>
+    </FilterWrapper>
+  );
+};
+
+Filter.propTypes = {
+  onChange: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired,
+};
