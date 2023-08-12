61a86e4ce61acbb27836593d7d2c4905d3125f2d
Soundlover1984 <denys.kovtun@gmail.com>
2023-08-12 22:36:28 +0300

    Update contactSlice.js

--- src/redux/contactSlice.js
+++ src/redux/contactSlice.js
@@ -2,13 +2,14 @@ import { createSlice } from '@reduxjs/toolkit';
 import { nanoid } from 'nanoid';
 import { defaultContacts } from 'components/data/DefaultContacts';
 
+const id = nanoid();
+
 export const contactsSlice = createSlice({
   name: 'contacts',
   initialState: defaultContacts,
   reducers: {
     addContact: (state, { payload }) => {
       const { name, number } = payload;
-      const id = nanoid();
       const contact = { id, name, number };
       state.push(contact);
     },
